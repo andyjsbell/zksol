68b30227bfe404f9cf163237d83048cab14d4558
Andy Bell <andyjsbell@gmail.com>
2025-09-03 17:55:10 +0200

    execute bytecode

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,8 +1,13 @@
 use log::{debug, error, info};
 use risc0_zkvm::guest::env;
+use solana_sbpf::aligned_memory::AlignedMemory;
 use solana_sbpf::elf::Executable;
+use solana_sbpf::memory_region::{MemoryMapping, MemoryRegion};
+use solana_sbpf::vm::EbpfVm;
 use solana_sbpf::{program::BuiltinProgram, vm::Config};
 use std::sync::Arc;
+
+#[derive(Default)]
 pub struct SolanaContext {
     pub compute_units_remaining: u64,
     pub compute_units_consumed: u64, // Track total consumption for monitoring
@@ -49,6 +54,45 @@ fn main() {
             panic!("Failed to create executable: {:?}", e);
         }
     };
+    let sbpf_version = executable.get_sbpf_version();
+    let config = executable.get_config();
+    let stack_size = config.stack_size();
 
+    let mut stack = AlignedMemory::<{ solana_sbpf::ebpf::HOST_ALIGN }>::zero_filled(stack_size);
+    let stack_len = stack.len();
+
+    let heap_size = 32 * 1024;
+    let mut heap = AlignedMemory::<{ solana_sbpf::ebpf::HOST_ALIGN }>::zero_filled(heap_size);
+
+    let regions: Vec<MemoryRegion> = vec![
+        executable.get_ro_region(),
+        MemoryRegion::new_writable_gapped(
+            stack.as_slice_mut(),
+            solana_sbpf::ebpf::MM_STACK_START,
+            if !sbpf_version.dynamic_stack_frames() && config.enable_stack_frame_gaps {
+                config.stack_frame_size as u64
+            } else {
+                0
+            },
+        ),
+        MemoryRegion::new_writable(heap.as_slice_mut(), solana_sbpf::ebpf::MM_HEAP_START),
+    ];
+
+    let memory_mapping = match MemoryMapping::new(regions, config, sbpf_version) {
+        Ok(m) => m,
+        Err(e) => panic!("Failed to create memory regions: {:?}", e),
+    };
+
+    let mut vm = EbpfVm::new(
+        executable.get_loader().clone(),
+        sbpf_version,
+        &mut SolanaContext::default(),
+        memory_mapping,
+        stack_len,
+    );
+
+    let (instruction_count, result) = vm.execute_program(&executable, true);
+    info!("Instruction Count: {}", instruction_count);
+    info!("Result: {:?}", result);
     // env::commit(&input);
 }
